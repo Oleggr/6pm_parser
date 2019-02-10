0bcdf1a467d43d6c04c781c0887bb65ef892961b
oleggr <oleg.gr@outlook.com>
2019-02-10 22:15:58 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# 6pm_parser
+# 6pm parser
+
+Parser of 6pm.com. Part of telegram bot, which will notify you about sales
+
+>Readme to example of web-site parser
+
+---
+
+### Содержание
+
+- [About](#about)
+- [Usage](#usage)
+- [Sources](#sources)
+- [About me](#about-me)
+
+---
+
+## About
+
+Telegram bot, which can parse web page of 6pm.com and send actual
+info about sales.
+
+Soon readme will be updated
+
+[Up](#6pm-parser)
+
+---
+
+## Usage
+
+Soon readme will be updated
+
+[Up](#6pm-parser)
+
+---
+
+## Sources
+
+This [article](https://habr.com/ru/post/316666/) is very helpful if you want to get some info about
+usage of generators in creating telegram bots.
+
+Also [documentation to python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot/wiki/Introduction-to-the-API) needs to be read.
+
+And telegram bot api [description](https://core.telegram.org/bots/api) 
+
+[Up](#6pm-parser)
+
+---
+
+## About me
+
+- Telegram - [@grit4in](https://t.me/grit4in)
+- Website - [Oleg Gritchin](https://oleg.gritchin.ru)
+
+[Up](#6pm-parser)
---
+++ parser.py
@@ -0,0 +1,78 @@
+'''
+Made by Gritchin Oleg
+
+Parser of 6pm.com
+
+Part of sales bot for telegram
+Hisname is @DiscountNotificator_bot
+
+github: github.com/oleggr/6pm_parser
+telegram: t.me/grit4in
+'''
+
+
+import datetime
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+URL = 'https://www.6pm.com/null/.zso?s=percentOff/desc/'
+replyes = []
+
+
+def get_html(url):
+    response = urllib.request.urlopen(url)
+    return response.read()
+
+
+def get_html_from_file(filename='6pm.html'):
+
+    with open (filename, 'r') as f:
+        html = f.read()
+
+    return html
+
+
+def parse(html):
+
+    soup = BeautifulSoup(html, features="html.parser")
+
+    sales_page = soup.find('div', class_ = '_2AfQY')
+
+    articles = sales_page.find_all('article')
+    
+    for article in articles:
+
+        current_price = float(article.find('span', class_='_3VzBv _1kZOe').text.split('$')[1])
+        standart_price = float(article.find('span', class_='GftsQ').text.split('$')[1])
+        discount = 100 - round((current_price / standart_price) * 100, 2)
+        link = 'https://www.6pm.com' + article.a.get('href')
+        # name1 = article.find('p', class_ = '_1HOLv').span.text
+        name = article.find('p', class_ = '_1HOLv').span.text \
+                + ' ' \
+                + article.find('p', class_ = '_3BAWv').text
+
+        if discount > 90:
+            message = name + '\n' \
+                    + '*' + discount + '*\n' \
+                    + '[link](' + link + ')'
+
+            replyes.append(message)
+            
+
+        
+        # print()
+        # # print('name1: ' + name1)
+        # print('name: ' + name)
+        # print('\ncurrent_price: ' + str(current_price))
+        # print('\nstandart_price: ' + str(standart_price))
+        # print('\ndiscount: ' + str(discount) + '%')
+
+        # print()
+        # print('=======================')
+
+    return replyes
+
+
+# if __name__ == '__main__':
+#     parse(get_html_from_file())
